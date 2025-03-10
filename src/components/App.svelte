3ed9a8eab941c400a9414768e741b63252248ddf
fjiang316 <fejiang@ucsd.edu>
2025-03-09 19:26:05 -0700

    update content

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,17 +6,17 @@
   <h1>Developing a LLM-Driven Multi-Agent Framework for Multimodal Translation</h1>
 
   <p class="authors">
-    <a href="https://www.linkedin.com/in/feiyang-jiang/">Feiyang Jiang *</a>, <a href="#">Kristina Wu *</a>, 
-    <a href="#">Nick Jumaoas *</a>, <a href="#">Hao Zhang</a>
+    <a href="https://github.com/fjiang316">Feiyang Jiang *</a>, <a href="https://github.com/KristinaWu23">Kristina Wu *</a>, 
+    <a href="https://github.com/nljumaoas">Nick Jumaoas *</a>, <a href="https://cseweb.ucsd.edu/~haozhang/">Hao Zhang</a>
     <br> University of California, San Diego
   </p>
 
   <p class="subtitle">2025 Data Science Senior Capstone</p>
 
   <div class="buttons">
-    <a href="#" class="btn"> Paper</a>
+    <a href="https://drive.google.com/file/d/1kaKTGjza_5TAwowdz6FN26d_Bm8c3j_E/view?usp=drive_link" class="btn"> Paper</a>
     <a href="https://github.com/nljumaoas/dsc180b" class="btn"> Code</a>
-    <a href="#" class="btn"> Poster</a>
+    <a href="https://drive.google.com/file/d/1SGo3LI8U6_L-Rc5T-r2pMld7FOGKrkRS/view?usp=drive_link" class="btn"> Poster</a>
     <a href="https://github.com/mantra-inc/open-mantra-dataset" class="btn"> Data</a>
   </div>
 
@@ -30,9 +30,21 @@
     <p>Notably, although we aim to match the general format for evaluation purposes, our identified text fields have a much tighter fit to facilitate typesetting. Additionally, while the English translation of the pictured text is used to evaluate the overall accuracy of our translation framework, we expect differences due to subjective measures of fluency and reader perception.</p>
 
     <h2>Methods</h2>
-    <p>Our innovated method pipeline involves three stages with the collaboration of agents... (more to come)</p>
+    <p>Our innovated method pipeline involves three stages with the collaboration of agents, beginning with page processing where text is recognized and clustered, then moving on to leverage multi-agent collaboration for translation, and at last the typesetting stage to return a translated version of the page with translations replacing the original texts.</p>
     <img src="method_pipeline.png" alt="Method Pipeline Diagram" class="content-image">
     
+    <section class="translation subsection">
+      <h3>Multi-agent Context-aware Translation Design</h3>
+      <p> In this stage, we introduce a multi-agent system for context-aware translation, focusing on visual-heavy content like manga and comic books. The system combines Visual Language Models (VLMs) and a multi-agent collaborative framework to enhance translation accuracy and coherence, ensuring linguistic precision and contextual relevance.</p>
+      <ul>
+        <li><strong>Visual Context Analysis: </strong> A Visual Language Model (VLM) analyzes the page's visual context, extracting key elements like characters and settings. This information helps inform the translation process, ensuring the translation is aligned with the visual nuances crucial to the narrative. For our demonstration pipeline, we uses llava 7b as the VLM agent. However, it is a small sized model with limited ability for interpretation and reasoning. If replaced with other stronger VLMs, the performance would be better.</li>
+        <li><strong>Multi-agent Translation Framework: </strong> The translation is handled by a multi-agent system with three specialized agents: the Linguistic Specialist Agent for grammatical accuracy, the Cultural Context Specialist Agent for cultural relevance, and the Visual Context Specialist Agent for aligning the translation with visual elements. Each agent contributes to a well-rounded translation. </li>
+        <li><strong>Incorporation of Historical Context: </strong> For series with continuous storylines, the system incorporates historical context by tracking narrative elements across multiple pages. This ensures consistency in character names, plot, and themes, maintaining coherence throughout the series. </li>
+        <li><strong>Agent Collaboration and Verification: </strong> The three agents engage in a round-robin process, reviewing and critiquing each other's translations. This collaborative approach helps resolve discrepancies and refine the final translation, ensuring it is linguistically accurate, culturally appropriate, and visually coherent. This ensures the quality of final translation. </li>
+        <li><strong>Fast Translation for Demo: </strong> Although the regular translation process involves agent double checking on final translation for more natural outputs, this slows down latency and adds to the cost, which would not be convinient for users who want instant access and don't mind the translation quality that much. In order to accommodate the need of this group, we enabled fast translation mode where early stop is implemented based on complexity of text and context. </li>
+        </ul>
+    </section>
+
     <section class="typesetting subsection">
       <h3>Typesetting</h3>
       <p>In the final stage of our pipeline, we integrate translated text back into manga pages while preserving visual aesthetics and readability. Our system automates text removal and adaptive reformatting to ensure a natural and high-quality presentation.</p>
@@ -43,11 +55,25 @@
     </section>
 
     <h2>Evaluation</h2>
-    <p>For evaluation method, we conducted ... (more to come with text explanation and equations)</p>
+    <p>The evaluation of the pipeline is separated into two major parts, one on the accuracy of page processing stage in capturing the correct position of texts and extracting the text accurately, the other part is in assessing the translation quality in context awareness and nuance. To effectively assess the accuracy, coherence, and context-awareness of the multi-agent translation system, we took a combination of automatic metrics such as BERTScore, as well as human evaluation to determine how well the system preserves the emotional nuance and narrative consistency.</p>
     
-    <section class="machine eval">
-      <h3>Machine Evaluation</h3>
-      <p>To systematically assess the quality of translations, we incorporate Machine Evaluation using the ChatGPT web application with the GPT-4o model as an automated evaluator. This approach allows us to efficiently compare different translation outputs at scale.</p>
+    <section class="Translation">
+      <section class="bertscore">
+        <h3>BERTScore and Human Evaluation</h3>
+        <p>To effectively assess the accuracy, coherence, and context-awareness of the multi-agent translation system, we took a combination of automatic metrics such as BERTScore, as well as human evaluation to determine how well the system preserves the emotional nuance and narrative consistency.</p>
+        <img src="bert_score_formula.png" alt="Formula used for BertScore" class="eval-image">
+        <ul>
+          <li><strong>BERTScore</strong>: Capture semantic similarity, focusing on contextual accuracy and nuance. Evaluated by Bert F1-score. </li>
+          <li><strong>Human Evaluation</strong>: Monolingual and bilingual experts rating translation for fluency, accuracy, proximity to released human translation and cultural appropriateness, choosing one best overall translation from the results of our model and one randomly selected baseline model.  </li>
+        </ul>
+        <p>Below is an example of our human evaluation survey question. We have a total of 4 different versions of the survey, each consists of ten randomly selected pages with one of the option being our model's output and the other being a randomly selected baseline's output to reduce bias. The final percentage of preference was taken as metrics.</p>
+        <img src="survey_example.png" alt="example of human evaluation survey" class="eval-image">
+      </section>
+
+      <section class="machine eval">
+        <h3>Machine Evaluation</h3>
+        <p>To systematically assess the quality of translations, we incorporate Machine Evaluation using the ChatGPT web application with the GPT-4o model as an automated evaluator. This approach allows us to efficiently compare different translation outputs at scale.</p>
+      </section>
     </section>
 
     <section class="challenge of evaluation on typesetting">
@@ -59,20 +85,36 @@
 
     <h2>Results</h2>
     <p>The final result has shown that ... (will include a short summary of the result and also a few plots and tables to illustrate)</p>
-    <img src="table_metrics.png" alt="table of metrics evaluation" class="eval-image">
-    <img src="human_eval.png" alt="human evaluation result" class="eval-image">
+    <section class="Translate result">
+      <section class="bertscore">
+        <h3>Performance and BERTScore</h3>
+        <img src="bert_result.png" alt="table of metrics evaluation" class="eval-image">
+        <p>We evaluate our pipeline against three baselines that are effective in I2I translation tasks:</p>
+        <ul>
+          <li><strong>Comparison to mono-agent baseline</strong>: our multi-agent structure outperform the baseline with both models (llama3.1:8b and gpt-4-turbo) </li>
+          <li><strong>Comparison to Google Translate</strong>: out multi-agent structure using gpt-4-turbo outperforms google translate baseline. </li>
+        </ul>
+        <p>This finding has been validated with human evaluation with results shown below.</p>
+      </section>
 
-    <section class="machine eval result">
-      <h3>Machine Evaluation</h3>
-      <p>The results from the GPT-4o-based Machine Evaluation indicate that our model performed favorably in comparison to baseline models:</p>
-  
-      <ul>
-        <li><strong>90% preference</strong> over Google Translate, suggesting improvements in fluency and accuracy.</li>
-        <li><strong>65% preference</strong> over the mono-agent baseline, reflecting notable enhancements in translation quality.</li>
-      </ul>
+      <section class="human eval">
+        <h3>Human Evaluation Result</h3>
+        <img src="human-eval-result.png" alt="human evaluation result" class="eval-image">
+        <p>As shown in the plot, our model achieved a 67% winning rate against mono-agent structure, and a 86% winning rate against google translate.</p>
+      </section>
 
-      <img src="GPT_evaluation.jpeg" alt="LLM evaluation result" class="eval-image">
-      <p>These findings suggest that GPT-4o's automated evaluation aligns well with human judgment and provides valuable insights into translation quality assessment.</p>
+      <section class="machine eval result">
+        <h3>Machine Evaluation</h3>
+        <p>The results from the GPT-4o-based Machine Evaluation indicate that our model performed favorably in comparison to baseline models:</p>
+    
+        <ul>
+          <li><strong>90% preference</strong> over Google Translate, suggesting improvements in fluency and accuracy.</li>
+          <li><strong>65% preference</strong> over the mono-agent baseline, reflecting notable enhancements in translation quality.</li>
+        </ul>
+
+        <img src="GPT_evaluation.jpeg" alt="LLM evaluation result" class="eval-image">
+        <p>These findings suggest that GPT-4o's automated evaluation aligns well with human judgment and provides valuable insights into translation quality assessment.</p>
+      </section>
     </section>
 
     <h2>Conclusion</h2>
@@ -82,8 +124,6 @@
     <p>This research sets the foundation for sophisticated multimodal translation systems that balance linguistic accuracy with visual presentation.</p>
     
     <h2>References</h2>
-    
-    <p>Motivation</p>
   </section>
   
 </main>
@@ -170,6 +210,10 @@
     margin-top: 20px;
   }
 
+  .content h3 {
+    color:rgb(164, 193, 252);
+  }
+
   .content p {
     font-size: 1rem;
     color: #444;
@@ -192,5 +236,6 @@
     display: block; /* Prevents unwanted gaps */
     border-radius: 8px;
     object-fit: contain; /* Ensures it scales properly */
+    margin: auto;
   }
 </style>
